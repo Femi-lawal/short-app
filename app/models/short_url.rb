c1b4d53ab8f28d1ad73c8752a4c0953576be699a
femi-lawal <femilawal87@yahoo.com>
2021-11-15 23:18:45 +0100

    feat(encoder): implemented Base62 encoder to generate short_code from full_url

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -5,6 +5,15 @@ class ShortUrl < ApplicationRecord
   validate :validate_full_url
 
   def short_code
+    short_url = []
+    short_url_id = id
+    # convert id integer into base 62 format
+    while short_url_id.positive?
+      short_url.append(CHARACTERS[short_url_id % 62])
+      short_url_id /= 62
+    end
+    # reversing the short_url to complete base conversion
+    short_url.reverse.join
   end
 
   def update_title!
