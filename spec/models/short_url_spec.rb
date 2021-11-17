096943f719c1538269941a93fb355d755fb44f0d
femi-lawal <femilawal87@yahoo.com>
2021-11-17 18:00:56 +0100

    test(model): modified test to use run_callbacks so correct modified output is returned

--- spec/models/short_url_spec.rb
+++ spec/models/short_url_spec.rb
@@ -59,11 +59,13 @@ RSpec.describe ShortUrl, type: :model do
 
       it "has the short_code for id 1001" do
         short_url.update_column(:id, 1001)
+        short_url.run_callbacks :save
         expect(short_url.short_code).to eq("g9")
       end
 
       it "has the short_code for id for 50" do
         short_url.update_column(:id, 50)
+        short_url.run_callbacks :save
         expect(short_url.short_code).to eq("O")
       end
     end
