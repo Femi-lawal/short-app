39b621c257972dd1484ce0dfdc72dd4644835170
femi-lawal <femilawal87@yahoo.com>
2021-11-17 18:23:55 +0100

    test(api): modified top 100 test to check if array of less than or equals to 100 elements is returned

--- spec/controllers/short_urls_controller_spec.rb
+++ spec/controllers/short_urls_controller_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe ShortUrlsController, type: :controller do
 
     it "has a list of the top 100 urls" do
       get :index, format: :json
-
-      expect(parsed_response['urls']).to be_include(short_url.public_attributes)
+      expect(parsed_response['urls'].class).to be Array
+      expect(parsed_response['urls'].count).to be <= 100
     end
 
   end
