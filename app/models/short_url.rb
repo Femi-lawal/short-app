bf3ad37696a8f83da4ad18baa5a923d2d1b284a8
femi-lawal <femilawal87@yahoo.com>
2021-11-17 17:10:34 +0100

    feat(encoder): modified encoder to save short_code to database after save callback

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,12 +1,13 @@
 class ShortUrl < ApplicationRecord
   after_create :update_title!
+  after_save :update_short_code!
   scope :most_popular, -> { order(click_count: :desc).limit(100) }
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
   validate :validate_full_url
 
-  def short_code
+  def update_short_code!
     short_url = []
     short_url_id = id
     # convert id integer into base 62 format
@@ -15,7 +16,8 @@ class ShortUrl < ApplicationRecord
       short_url_id /= 62
     end
     # reversing the short_url to complete base conversion
-    short_url.reverse.join
+    base62_url = short_url.reverse.join
+    update_column(:short_code, base62_url)
   end
 
   def update_count!
@@ -35,7 +37,7 @@ class ShortUrl < ApplicationRecord
       parsed_url = URI.parse(full_url)
       errors.add(:full_url, 'url must conform to http or https') unless parsed_url.is_a?(URI::HTTP)
     rescue URI::InvalidURIError
-      errors.add(:full_url, 'Full url is not a valid url')
+      errors.add(:full_url, 'is not a valid url')
     end
     url_regex = %r{^((http|https)://)?[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$}ix
     errors.add(:full_url, 'Full url is not a valid url') unless full_url.match(url_regex)
